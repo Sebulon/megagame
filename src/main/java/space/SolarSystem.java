b948bc5bbde78d6a644d6cd5bb07a0d40bd10381
Robert <robert.sahlqvist@hotmail.com>
2022-03-09 18:36:07 +0100

    More space stuff

--- src/main/java/space/SolarSystem.java
+++ src/main/java/space/SolarSystem.java
@@ -5,4 +5,9 @@ import java.util.ArrayList;
 public class SolarSystem {
     private int size;
     private ArrayList<ICelestialObject> objectList;
+
+    public SolarSystem (int size, Star star){
+        this.size = size;
+        objectList.add(star);
+    }
 }
