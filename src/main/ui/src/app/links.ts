3d5afd33bbbda4214b7917cb211c6793f3536255
Samuel Dahlborg <beaosam@gmail.com>
2022-03-09 14:14:30 +0100

    route update to frontend to match backend

--- src/main/ui/src/app/links.ts
+++ src/main/ui/src/app/links.ts
@@ -2,8 +2,8 @@
  * Data class with links to different backend calls
  */
 export class Links {
-  public static ships = 'api/allShips';
-  public static playerShip = 'api/playerShip';
-  public static postShip = 'api/allShips';
+  public static ships = 'api/ships/allShips';
+  public static playerShip = 'api/ships/playerShip';
+  public static postShip = 'api/ships/allShips';
   public static users = 'api/users';
 }
