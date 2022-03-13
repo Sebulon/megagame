775cf5c73540933535fa8b218e9541fa53867693
Oscar Palmqvist <oscpal@student.chalmers.se>
2022-03-13 16:53:35 +0100

    Resources spring-integrated. PlayerShip clarification.

--- src/main/java/com/chalmersmegagame/game/ships/ShipService.java
+++ src/main/java/com/chalmersmegagame/game/ships/ShipService.java
@@ -1,5 +1,6 @@
 package com.chalmersmegagame.game.ships;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.chalmersmegagame.game.MainGame;
@@ -21,6 +22,16 @@ public class ShipService {
         return getAllShips().stream().filter(t -> t.getName().equals(shipName)).findFirst().get();
     }
 
+    public PlayerShip getPlayerShipByName(String shipName){
+        Ship ship = getShipByName(shipName);
+        if(ship instanceof PlayerShip){
+            PlayerShip playerShip = (PlayerShip) ship;
+            return playerShip;
+        }else{
+            throw new RuntimeException("No PlayerShip with name " + shipName + " found");
+        }
+    }
+
     public void addShip(Ship ship) {
         game.addShip(ship);
     }
@@ -32,5 +43,18 @@ public class ShipService {
     public void deleteShip(String shipName) {
         Ship shipToDelete = getShipByName(shipName);
         getAllShips().remove(shipToDelete);
+    }
+
+    public HashMap<String, Integer> getPlayerShipResourceQuantities(String shipName){
+        return getPlayerShipByName(shipName).getResourceQuantities();
+    }
+
+    public void modifyPlayerShipResource(String shipName, String resource, int quantity){
+        PlayerShip ship = getPlayerShipByName(shipName);
+        if(quantity > 0){
+            ship.addResource(resource, quantity);
+        }else if(quantity < 0){
+            ship.removeResource(resource, quantity);
+        }
     }
 }
