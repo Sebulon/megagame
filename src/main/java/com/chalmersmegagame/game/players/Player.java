9c5708abbfeeb755ba592a0b13fc5260c1c5ba42
DwarfMamba2 <oscpal@student.chalmers.se>
2022-03-13 19:38:39 +0100

    Player JPA-integrated

--- src/main/java/com/chalmersmegagame/game/players/Player.java
+++ src/main/java/com/chalmersmegagame/game/players/Player.java
@@ -1,14 +1,20 @@
 package com.chalmersmegagame.game.players;
 
+import javax.persistence.*;
+
 import com.chalmersmegagame.game.ships.Ship;
 
 /**
  * Class representing a player and at what ship it is on.
  */
+
+ @Entity
 public class Player {
 
+    @Id
     public final String id;
-
+    
+    @Transient
     private Ship boardedShip;
 
     public Player(String id) {
