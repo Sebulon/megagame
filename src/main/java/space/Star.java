7f0c778fa8b53b3d5bbd781801bb53fa96131b95
Robert <robert.sahlqvist@hotmail.com>
2022-03-15 22:41:35 +0100

    Update Star.java

--- src/main/java/space/Star.java
+++ src/main/java/space/Star.java
@@ -3,33 +3,12 @@ package space;
 
 public class Star extends CelestialBody {
 
-    private String stellarClass;
-    /*public Star(int size) {
-        this.type = "Star";
-        this.index = 0;
-        this.size = size;
-    }
-
-    public Star(int size, int index){
-        this.type = "Star";
-        this.index = index;
-        this.size = size;
-    }*/
-
-    public Star(int size, int index, String type){
+    public Star(int size, String type){
         if (!typeHandler.getStarTypes().contains(type)){
             throw new IllegalArgumentException("Illegal type");
         }
-        this.index = index;
         this.size = size;
         this.type = type;
     }
 
-    public String getStellarClass() {
-        return this.stellarClass;
-    }
-
-    public void setStellarClass(String stellarClass) {
-        this.stellarClass = stellarClass;
-    }
 }
