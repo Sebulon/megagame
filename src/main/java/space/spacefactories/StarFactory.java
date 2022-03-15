b30db98b6101052a484be22bbcf419a988d30f39
Robert <robert.sahlqvist@hotmail.com>
2022-03-15 17:24:00 +0100

    Update StarFactory.java

--- src/main/java/space/spacefactories/StarFactory.java
+++ src/main/java/space/spacefactories/StarFactory.java
@@ -1,4 +1,19 @@
 package space.spacefactories;
 
+import space.Star;
+
 public class StarFactory {
+
+    public Star createStandardStar(int size){
+        return new Star(size,0,"Star");
+    }
+
+    public Star createStandardStarWithIndex(int size, int index){
+        return new Star(size, index, "Star");
+    }
+
+    public Star createStar(int size, int index, String type){
+        return new Star(size, index, type);
+    }
+    
 }
