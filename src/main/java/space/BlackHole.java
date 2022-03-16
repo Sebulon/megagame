f7122a53b8d0def14d6a64f92bc0053e8b051e9c
Robert <robert.sahlqvist@hotmail.com>
2022-03-16 13:25:57 +0100

    Create BlackHole.java

---
+++ src/main/java/space/BlackHole.java
@@ -0,0 +1,8 @@
+package space;
+
+public class BlackHole extends CelestialBody implements ICelestialObject{
+    public BlackHole (int size){
+        this.size = size;
+        this.type = "Black Hole";
+    }
+}
