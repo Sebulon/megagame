23adfe14579ef85da303c06dc25e58a272e5e3cf
Robert <robert.sahlqvist@hotmail.com>
2022-03-16 13:26:02 +0100

    Update SolarSystem.java

--- src/main/java/space/SolarSystem.java
+++ src/main/java/space/SolarSystem.java
@@ -7,16 +7,16 @@ public class SolarSystem {
     private int size;
     private ArrayList<ICelestialObject> objectList = new ArrayList<>();
 
-    public SolarSystem (int size, Star star){
+    public SolarSystem (int size, IGravityWell gravityWell){
         this.size = size;
-        objectList.add(star);
+        objectList.add(gravityWell);
     }
 
     public SolarSystem (int size, ArrayList<ICelestialObject> objectList){
         if (objectList.size() > size + 1){
             throw new IllegalArgumentException("The specified solar system is too large");
         }
-        if (!(objectList.get(0) )){
+        if (!(objectList.get(0) instanceof IGravityWell)){
             throw new IllegalArgumentException("The solar system needs a strong gravity well");
         }
         this.size = size;
