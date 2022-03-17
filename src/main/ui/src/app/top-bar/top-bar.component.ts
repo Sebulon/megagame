63e910fece09cafbc1605168e572927c22b8ae24
Sebulon <sebasjoh@student.chalmers.se>
2022-03-17 03:11:37 +0100

    Added navigation in top-bar

--- src/main/ui/src/app/top-bar/top-bar.component.ts
+++ src/main/ui/src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-top-bar',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
-  constructor() { }
+  routes: { name: string, link: string }[] = [];
+
+  constructor(router: Router) {
+    router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(e => this.updateRoute((e as NavigationEnd).url))
+  }
 
   ngOnInit(): void {
+  }
+
+  // TODO: more modular?
+  updateRoute(url: string) {
+    console.log(url);
+    if (url.includes('controller-view')) {
+      let id = url.split('/')[1];
+      let prefix = id + '/controller-view';
+      this.routes = [
+        {name: 'Overview', link: prefix},
+        {name: 'Ships', link: prefix + '/ships'},
+        {name: 'Players', link: prefix + '/players'}
+      ];
+    } else if (url.includes('player-view')) {
+      let id = url.split('/')[1];
+      let prefix = id + '/player-view';
+      this.routes = [{name: 'Overview', link: prefix}];
+    } else {
+      this.routes = [];
+    }
   }
 
 }
--- src/main/ui/src/app/user.service.ts
+++ src/main/ui/src/app/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 import {Links} from "./links";
+import {UserRole} from "./objects/user-role";
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,18 @@ export class UserService {
 
   getId(route: ActivatedRoute) {
     return route.snapshot.paramMap.get('id');
+  }
+
+  getRole(): UserRole | null {
+    let tmp = localStorage.getItem('role');
+    if (!tmp) return null;
+    switch (tmp) {
+      case 'controller':
+        return {role: 'controller'};
+      case 'player':
+        return {role: 'player'};
+      default:
+        throw new Error("Saved role is not a user role!");
+    }
   }
 }
