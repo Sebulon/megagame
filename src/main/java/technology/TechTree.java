aedb2f9aaa9228f3b7d89b6c4a81004362c79273
Robert <robert.sahlqvist@hotmail.com>
2022-03-25 11:50:05 +0100

    Update TechTree.java

--- src/main/java/technology/TechTree.java
+++ src/main/java/technology/TechTree.java
@@ -1,4 +1,13 @@
 package technology;
 
+import java.util.ArrayList;
+
 public class TechTree {
+    private ArrayList<Technology> technologies;
+    private ArrayList<Technology> researched;
+    private ArrayList<Technology> available;
+
+    public TechTree(){
+
+    }
 }
