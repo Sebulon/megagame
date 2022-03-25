1604f888bea4af46af9b22ebe711d8e1b03248ac
Robert <robert.sahlqvist@hotmail.com>
2022-03-25 12:24:21 +0100

    Update TechTree.java

--- src/main/java/technology/TechTree.java
+++ src/main/java/technology/TechTree.java
@@ -8,6 +8,43 @@ public class TechTree {
     private ArrayList<Technology> available;
 
     public TechTree(){
+        technologies = new ArrayList<>();
+        researched = new ArrayList<>();
+        available = new ArrayList<>();
+    }
 
+    public TechTree(ArrayList<Technology> technologies){
+        this.technologies = technologies;
+        researched = new ArrayList<>();
+        available = new ArrayList<>();
+    }
+
+    public void update (){
+        for (Technology technology : technologies){
+            if (technology.isResearchable()){
+                if (!available.contains(technology)) {
+                    available.add(technology);
+                }
+            }
+
+            if (technology.isResearched()){
+                if (!researched.contains(technology)){
+                    researched.add(technology);
+                }
+            }
+
+            if (available.contains(technology)){
+                if (technology.isResearched()){
+                    available.remove(technology);
+                }
+            }
+
+            if (researched.contains(technology)){
+                if(technology.isResearchable()){
+                    researched.remove(technology);
+                }
+            }
+
+        }
     }
 }
