3132a55eb8fb405b7706834ca5c97214f0985097
Robert <robert.sahlqvist@hotmail.com>
2022-03-25 11:50:08 +0100

    Update Technology.java

--- src/main/java/technology/Technology.java
+++ src/main/java/technology/Technology.java
@@ -3,9 +3,27 @@ package technology;
 import java.util.ArrayList;
 
 public abstract class Technology {
-    private boolean researched;
-    private ArrayList <Technology> requirements;
+    private boolean researched, researchable;
+    private ArrayList <Technology> requirements = new ArrayList<>();
     private String description;
+
+    public void update (){
+        if (researched == true){
+            researchable = false;
+        }
+
+        if (requirements.isEmpty()){
+            researchable = true;
+        }
+
+        for (Technology tech: requirements){
+            tech.update();
+
+            if (tech.isResearched() == true){
+                requirements.remove(tech);
+            }
+        }
+    }
 
     public boolean isResearched() {
         return researched;
@@ -13,6 +31,14 @@ public abstract class Technology {
 
     public void setResearched(boolean researched) {
         this.researched = researched;
+    }
+
+    public boolean isResearchable() {
+        return researchable;
+    }
+
+    public void setResearchable(boolean researchable) {
+        this.researchable = researchable;
     }
 
     public ArrayList<Technology> getRequirements() {
