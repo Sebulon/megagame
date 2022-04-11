fb794832c028d97a3d0c4a211f2f6cf67e0051fb
Robert <robert.sahlqvist@hotmail.com>
2022-04-11 17:13:29 +0200

    Update Galaxy.java

--- src/main/java/space/Galaxy.java
+++ src/main/java/space/Galaxy.java
@@ -1,4 +1,16 @@
 package space;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Galaxy {
+    private Set<SolarSystem> solarSystems;
+
+    public Galaxy(Set<SolarSystem> solarSystems){
+        this.solarSystems = solarSystems;
+    }
+
+    public void pairSolarSystems(SolarSystem solarSystem1, SolarSystem solarSystem2){
+        solarSystem1.addConnection(solarSystem2);
+    }
 }
