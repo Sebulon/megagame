d782e25f6a848daa55b142ed91d6e22a89de7d6d
Robert <robert.sahlqvist@hotmail.com>
2022-04-21 21:24:55 +0200

    Update MaxHPTech.java

--- src/main/java/com/chalmersmegagame/game/technology/technologies/MaxHPTech.java
+++ src/main/java/com/chalmersmegagame/game/technology/technologies/MaxHPTech.java
@@ -1,8 +1,23 @@
 package com.chalmersmegagame.game.technology.technologies;
 
 import com.chalmersmegagame.game.ships.Ship;
+import com.chalmersmegagame.game.technology.Technology;
 
-public abstract class MaxHPTech {
+public abstract class MaxHPTech extends Technology {
     private Ship ship;
+    private int newMaxHP;
+
+    public MaxHPTech (Ship ship, int newMaxHP){
+        if (newMaxHP < this.ship.getMaxHP()){
+            throw new IllegalArgumentException("The new Max HP is too low");
+        }
+        this.ship = ship;
+        this.newMaxHP = newMaxHP;
+    }
+    @Override
+    public void activate (){
+        ship.setMaxHP(newMaxHP);
+        this.setResearched(true);
+    }
 
 }
