c4f0ab7acc020f9949ab938025343e9017654cad
Robert <robert.sahlqvist@hotmail.com>
2022-04-21 13:44:27 +0200

    Removed reduntant folders

--- src/main/java/space/BlackHole.java
+++
@@ -1,9 +0,0 @@
-package space;
-
-public class BlackHole extends CelestialBody implements ICelestialObject{
-
-    public BlackHole (int size){
-        this.size = size;
-        this.type = "Black Hole";
-    }
-}
