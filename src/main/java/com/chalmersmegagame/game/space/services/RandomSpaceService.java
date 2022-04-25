af7ecdf6c082dd5a469453c8bce909787d373707
Robert <robert.sahlqvist@hotmail.com>
2022-04-25 16:19:05 +0200

    Create RandomSpaceService.java

---
+++ src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
@@ -0,0 +1,31 @@
+package com.chalmersmegagame.game.space.services;
+
+import com.chalmersmegagame.game.space.Galaxy;
+import com.chalmersmegagame.game.space.IGravityWell;
+import com.chalmersmegagame.game.space.Planetoid;
+import com.chalmersmegagame.game.space.SolarSystem;
+
+public class RandomSpaceService {
+    private SolarSystemService solarSystemService = new SolarSystemService();
+
+    public SolarSystem generateRandomSolarSystem(){
+
+    }
+
+    public Planetoid generateRandomPlanetoid(){
+
+
+    }
+
+    public IGravityWell generateRandomGravityWell(){
+
+
+    }
+
+    public Galaxy generateRandom(){
+
+
+    }
+
+
+}
