217daa394431d1984aa10403da5b013e7a2f0a4e
Robert <robert.sahlqvist@hotmail.com>
2022-04-25 22:12:18 +0200

    Update RandomSpaceService.java

--- src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
+++ src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
@@ -1,28 +1,48 @@
 package com.chalmersmegagame.game.space.services;
 
-import com.chalmersmegagame.game.space.Galaxy;
-import com.chalmersmegagame.game.space.IGravityWell;
-import com.chalmersmegagame.game.space.Planetoid;
-import com.chalmersmegagame.game.space.SolarSystem;
+import com.chalmersmegagame.game.space.*;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class RandomSpaceService {
     private SolarSystemService solarSystemService = new SolarSystemService();
+
+    public IGravityWell generateRandomGravityWell(){
+        Random size = new Random();
+        DiceService diceService = new DiceService();
+        diceService.addDie(6);
+        int result = diceService.rollSum();
+        if (result == 1){
+            return new BlackHole(size.nextInt(10));
+        }
+        else{
+            return generateRandomStar();
+        }
+    }
+
+    public Planetoid generateRandomPlanetoid(){
+
+    }
+
+    public Star generateRandomStar(){
+        Random size = new Random();
+        DiceService diceService = new DiceService();
+        diceService.addDie(20);
+        int result = diceService.rollSum();
+        if (result == 1){
+            return new Star(size.nextInt(10), "Pulsar");
+        }
+        else if (result == 2){
+            return new Star(size.nextInt(10), "Neutron Star");
+        }
+    }
 
     public SolarSystem generateRandomSolarSystem(){
 
     }
 
-    public Planetoid generateRandomPlanetoid(){
-
-
-    }
-
-    public IGravityWell generateRandomGravityWell(){
-
-
-    }
-
-    public Galaxy generateRandom(){
+    public Galaxy generateRandomGalaxy(){
 
 
     }
