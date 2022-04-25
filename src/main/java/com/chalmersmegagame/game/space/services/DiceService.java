ac64d8ca2c7b3198b5608c07b72f99302aef2eac
Robert <robert.sahlqvist@hotmail.com>
2022-04-25 22:12:15 +0200

    Update DiceService.java

--- src/main/java/com/chalmersmegagame/game/space/services/DiceService.java
+++ src/main/java/com/chalmersmegagame/game/space/services/DiceService.java
@@ -5,51 +5,52 @@ import java.util.Random;
 
 public class DiceService {
 
-    ArrayList <Die> dice = new ArrayList<>();
+    private final ArrayList<Die> dice = new ArrayList<>();
 
-    private class Die{
-
-        private int type;
-
-        public Die (int type){
-            this.type = type;
-        }
-
-        public int roll(){
-            Random random = new Random();
-            return random.nextInt(type + 1);
-        }
+    public void addDie(int type) {
+        dice.add(new Die(type));
     }
 
-    public Die die (int type){
-        return new Die(type);
-    }
-
-    public ArrayList<Die> intArrayToDice (int[] array){
-        ArrayList<Die> dice = new ArrayList<>();
-        for (int i = 0; i < array.length; i++){
+    public ArrayList<Die> intArrayToDice(int[] array) {
+        dice.clear();
+        for (int i = 0; i < array.length; i++) {
             dice.add(new Die(array[i]));
         }
         return dice;
     }
 
-    public int rollSum(ArrayList<Die> dice){
+    public int rollSum() {
         int sum = 0;
-        for (Die die : dice){
+        for (Die die : dice) {
             sum += die.roll();
         }
         return sum;
     }
 
-    public int rollKeepHighest(ArrayList<Die> dice){
+    public int rollKeepHighest() {
         int returnInt = 0;
-        for (Die die : dice){
-            if (returnInt < die.roll()){
+        for (Die die : dice) {
+            if (returnInt < die.roll()) {
                 returnInt = die.roll();
             }
         }
         return returnInt;
     }
+
+    private class Die {
+
+        private final int type;
+
+        public Die(int type) {
+            this.type = type;
+        }
+
+        public int roll() {
+            Random random = new Random();
+            return random.nextInt(type + 1);
+        }
+    }
+
 }
 
 
