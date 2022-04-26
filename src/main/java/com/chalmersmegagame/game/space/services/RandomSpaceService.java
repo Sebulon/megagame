e8286fb779aea9ec7e8020564baec1b03689f1a2
Robert <robert.sahlqvist@hotmail.com>
2022-04-26 22:31:35 +0200

    Update RandomSpaceService.java

--- src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
+++ src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
@@ -21,30 +21,53 @@ public class RandomSpaceService {
         }
     }
 
-    public Planetoid generateRandomPlanetoid(){
+    public Feature generateRandomHabitableFeature(){
+        return null;
+    }
 
+    public Planetoid generateRandomPlanetoid(){
+        return null;
     }
 
     public Star generateRandomStar(){
-        Random size = new Random();
+        Random random = new Random();
+        int size = random.nextInt(10) + 1;
         DiceService diceService = new DiceService();
-        diceService.addDie(20);
+        diceService.intArrayToDice(new int[]{20,20});
         int result = diceService.rollSum();
-        if (result == 1){
-            return new Star(size.nextInt(10) + 1, Star.StarTypes.PULSAR);
+        if (result <= 3){
+            return new Star(size, Star.StarTypes.PULSAR);
         }
-        else if (result == 2){
-            return new Star(size.nextInt(10) + 1, Star.StarTypes.NEUTRON);
+        else if (result == 4){
+            return new Star(size, Star.StarTypes.NEUTRON);
         }
+        else if (result <= 8 ){
+            return new Star(size, Star.StarTypes.BROWN_DWARF);
+        }
+        else if (result <= 16 ){
+            return new Star(size, Star.StarTypes.ORANGE);
+        }
+        else if (result <= 19){
+            return new Star(size, Star.StarTypes.BLUE);
+        }
+        else if (result <= 27){
+            return new Star(size, Star.StarTypes.YELLOW);
+        }
+        else if (result <= 34){
+            return new Star(size, Star.StarTypes.RED);
+        }
+        else if (result <= 40){
+            return new Star(size, Star.StarTypes.WHITE);
+        }
+        return null;
     }
 
     public SolarSystem generateRandomSolarSystem(){
-
+        return null;
     }
 
     public Galaxy generateRandomGalaxy(){
-
-
+        return null;
     }
 
 
