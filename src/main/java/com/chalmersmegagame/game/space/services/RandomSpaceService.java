fe47d2fc936337cb0c0a272a70f6f03d013f31cc
Robert <robert.sahlqvist@hotmail.com>
2022-05-02 20:11:19 +0200

    Update RandomSpaceService.java

--- src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
+++ src/main/java/com/chalmersmegagame/game/space/services/RandomSpaceService.java
@@ -8,33 +8,108 @@ import java.util.Random;
 public class RandomSpaceService {
     private SolarSystemService solarSystemService = new SolarSystemService();
 
+    private int randomNumber (int max){
+        Random random = new Random();
+        return random.nextInt(max) + 1;
+    }
+
     public IGravityWell generateRandomGravityWell(){
-        Random size = new Random();
-        DiceService diceService = new DiceService();
-        diceService.addDie(6);
-        int result = diceService.rollSum();
+        int result = randomNumber(6);
         if (result == 1){
-            return new BlackHole(size.nextInt(1) + 1);
+            return new BlackHole(randomNumber(10));
         }
         else{
             return generateRandomStar();
         }
     }
 
-    public Feature generateRandomHabitableFeature(){
-        return null;
-    }
+    public Planetoid generateRandomPlanetoid(int maxSize){
+        int size = randomNumber(maxSize);
+        int result = randomNumber(20);
+        if (result == 1){
+            Planetoid habitable = new Planetoid(size, Planetoid.PlanetoidTypes.HABITABLE);
+            habitable.addFeature(generateRandomHabitableFeature());
+            habitable.addResource("Food", randomNumber(10));
+            habitable.addResource("Water", randomNumber(10));
+            habitable.addResource("Minerals", Math.max(randomNumber(10) - 5, 0));
+            habitable.addResource("Gasses", Math.max(randomNumber(10) - 5, 0));
+            habitable.addResource("Chemicals", Math.max(randomNumber(10) - 5, 0));
+            habitable.addResource("Alien Animals", randomNumber(5));
+            habitable.addResource("Alien Plants", randomNumber(5));
 
-    public Planetoid generateRandomPlanetoid(){
+            if(randomNumber(6) > 4){
+                habitable.addSatellite(generateRandomPlanetoid(habitable.getSize()-1));
+            }
+            return habitable;
+        }
+        else if (result == 2){
+            Planetoid apocalyptic = new Planetoid(size, Planetoid.PlanetoidTypes.APOCALYPTIC);
+            apocalyptic.addResource("Artifacts", Math.max(randomNumber(10) - 5, 0));
+            apocalyptic.addResource("Minerals", randomNumber(10));
+            apocalyptic.addResource("Weapons of Mass Destruction", Math.max(randomNumber(10) - 7, 0));
+
+            if (randomNumber(6) > 4){
+                apocalyptic.addSatellite(generateRandomPlanetoid(apocalyptic.getSize() - 1));
+            }
+            return apocalyptic;
+        }
+        else if (result == 3){
+            Planetoid broken = new Planetoid(size, Planetoid.PlanetoidTypes.BROKEN);
+            broken.addResource("Minerals", randomNumber(20));
+
+            if (randomNumber(6) > 4){
+                broken.addSatellite(generateRandomPlanetoid(broken.getSize() - 1));
+            }
+            return broken;
+        }
+        else if (result <= 5 ){
+            Planetoid molten = new Planetoid(size, Planetoid.PlanetoidTypes.MOLTEN);
+            molten.addResource("Energy", randomNumber(20));
+            molten.addResource("Minerals", randomNumber(10));
+            molten.addResource("Chemicals", randomNumber(10));
+
+            if (randomNumber(6) > 4){
+                molten.addSatellite(generateRandomPlanetoid(molten.getSize() - 1));
+            }
+        }
+        else if (result <= 8){
+            Planetoid toxic = new Planetoid(size, Planetoid.PlanetoidTypes.TOXIC);
+            toxic.addResource("Gasses", randomNumber(10));
+            toxic.addResource("Chemicals", randomNumber(10));
+            toxic.addResource("Minerals", randomNumber(10));
+            toxic.addResource("Alien Microbes", Math.max(randomNumber(10) - 8, 0));
+
+            if (randomNumber(6) > 4){
+                toxic.addSatellite(generateRandomPlanetoid(toxic.getSize() - 1));
+            }
+            return toxic;
+        }
+        else if (result <= 11){
+            Planetoid frozen = new Planetoid(size, Planetoid.PlanetoidTypes.FROZEN);
+            frozen.addResource("Water", randomNumber(10));
+            frozen.addResource("Liquid Gasses",randomNumber(20));
+            frozen.addResource("Alien Animals", Math.max(randomNumber(10) - 8, 0));
+
+            if (randomNumber(6) > 4){
+                frozen.addSatellite(generateRandomPlanetoid(frozen.getSize() - 1));
+            }
+        }
+        else if (result <= 14){
+
+        }
+        else if (result <= 17){
+
+        }
+        else if (result <= 20){
+
+        }
         return null;
     }
 
     public Star generateRandomStar(){
-        Random random = new Random();
-        int size = random.nextInt(10) + 1;
-        DiceService diceService = new DiceService();
-        diceService.intArrayToDice(new int[]{20,20});
-        int result = diceService.rollSum();
+        int size = randomNumber(10);
+        int result = randomNumber(20);
+        result += randomNumber(20);
         if (result <= 3){
             return new Star(size, Star.StarTypes.PULSAR);
         }
@@ -42,7 +117,7 @@ public class RandomSpaceService {
             return new Star(size, Star.StarTypes.NEUTRON);
         }
         else if (result <= 8 ){
-            return new Star(size, Star.StarTypes.BROWN_DWARF);
+            return new Star(randomNumber(5), Star.StarTypes.BROWN_DWARF);
         }
         else if (result <= 16 ){
             return new Star(size, Star.StarTypes.ORANGE);
@@ -62,13 +137,16 @@ public class RandomSpaceService {
         return null;
     }
 
-    public SolarSystem generateRandomSolarSystem(){
+    public SolarSystem generateRandomSolarSystem(int size){
         return null;
     }
 
-    public Galaxy generateRandomGalaxy(){
+    public Galaxy generateRandomGalaxy(int size){
         return null;
     }
 
+    public Feature generateRandomHabitableFeature(){
+        return null;
+    }
 
 }
